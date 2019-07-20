54cc7a3d6f4bc887394875e6aa58858b50c33c91
Adrian Maulana Muhammad <adriantoto7@gmail.com>
2019-07-20 20:31:36 +0700

    MainActivity: override onBackpressed

--- app/src/main/java/dicoding/adrian/submission3/MainActivity.java
+++ app/src/main/java/dicoding/adrian/submission3/MainActivity.java
@@ -2,12 +2,32 @@ package dicoding.adrian.submission3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variable back pressed
+    private long backPressedTime;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // onBackPressed untuk exit
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
+
 }
