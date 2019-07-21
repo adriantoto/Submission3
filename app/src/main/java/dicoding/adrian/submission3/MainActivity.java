d97196c4c97de929650a146504fe00da7b36d62a
Adrian Maulana Muhammad <adriantoto7@gmail.com>
2019-07-21 12:52:05 +0700

    adding fragments, bottom nav bar, changing nav bar and action bar color

--- app/src/main/java/dicoding/adrian/submission3/MainActivity.java
+++ app/src/main/java/dicoding/adrian/submission3/MainActivity.java
@@ -1,16 +1,47 @@
 package dicoding.adrian.submission3;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import dicoding.adrian.submission3.Movie.MovieFragment;
+import dicoding.adrian.submission3.TV.TvFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     // Variable back pressed
     private long backPressedTime;
     private Toast backToast;
+
+    // Bottom Navigation
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment;
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_movie:
+                    fragment = new MovieFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frame_container, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.navigation_tv:
+                    fragment = new TvFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frame_container, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
+
+        // ActionBar Gradient
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.mygradient));
+        }
+
+        // Bottom Navigation Declaration
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // The first fragment to show when open the app
+        if (savedInstanceState == null) {
+            navigation.setSelectedItemId(R.id.navigation_movie);
+        }
     }
 
     // onBackPressed untuk exit
